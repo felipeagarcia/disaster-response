002380f9345d43344753eb9e9fa35b04bb5ce7bf
Felipe Aparecido Garcia <felipegarciaitu@hotmail.com>
2021-03-16 02:21:34 -0300

    Create machine learning pipeline
    Define NLP and machine learning pipeline, train and evaluate model, performinc GridSearchCV

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,104 @@
 import sys
+import re
+import pandas as pd
+import numpy as np
+
+from joblib import dump
+
+from sqlalchemy import create_engine
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
 
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine('sqlite:///{}.db'.format(database_filepath))
+    df = pd.read_sql("SELECT * FROM disaster_response", engine)
+    X = df['message']
+    Y = df.drop(columns=['message', 'original', 'genre', 'id'])
+    category_names = Y.columns
+    return np.array(X), np.array(Y), category_names
 
 def tokenize(text):
-    pass
+    normalized_text = re.sub(r'[^a-zA-Z0-9]',' ', text.lower())
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()), 
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__max_df': (0.75, 1.0),
+        'vect__max_features': (None, 5000, 10000),
+        'tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    labels = np.unique(y_pred)
+    accuracy = (y_pred == Y_test).mean()
+
+    print("Labels:", labels)
+    print("Accuracy:", accuracy)
+    print("\nBest Parameters:", model.best_params_)
 
 
 def save_model(model, model_filepath):
-    pass
+    dump(model, '{}.joblib'.format(model_filepath))
 
 
 def main():
